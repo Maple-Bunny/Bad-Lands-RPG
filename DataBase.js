42e62381ec2c41ac620dd18a7e0b3b049c920801
Maple-Bunny <lonikaida@gmail.com>
2024-11-05 14:05:03 -0300

    Feita varias mudança no código principal e alterado o layout.

--- DataBase.js
+++ DataBase.js
@@ -393,7 +393,7 @@ const ClassDB = [
 	{
 		Classe: "Defensor",
 		Status: "[+2 de Resistência] [+1 em Armadura]",
-    Icon: "brasao_ Engenheiro.png",
+    Icon: "defender.png",
 		Descrição: "O Defensor protege a equipe e tem vários recursos para se proteger e resistir a um grande número de danos. Defensores usam uma armadura mais espessa, por isso possuem uma mobilidade menor, possui canhões de ombro e grande resistência a impactos.",
 		Itens_Iniciais: "Armadura Pesada de Cadete (+1 Armadura, valor $50). [ Escopeta AT-07 (Dano 3d4, Distância 10 Metros, valor $40). Ou Espada Longa de bronze (Dano 1d10, valor $25) ] Canhão de ombro TX-MK1 (Dano 1d8, Distância 15 Metros, Recarga 2 turnos, valor $65). Mochila com 4 Espaços. X2 Analgésicos (cura 2d4 se ingerido). $10 de dinheiro.",
 		Habilidades_por_Nivel: "Nível 1: Pele de Pedra. Nível 2: Ignição. Ponto Delta. Nível 3: Toque Gelado Nível 4: Eletrificar Lâmina. Mega Broca. Ponto Delta. Nível 5: Escudo Sinético. Nível 6: Soco Foguete.  Ponto Delta. Nível 7: Canhão Umbral. Nível 8: Força de Hércules. Ponto Delta. Nível 9: Canhão Ômega. Nível 10: Forma Delta. Ponto Delta."
@@ -401,7 +401,7 @@ const ClassDB = [
 	{
 		Classe: "Engenheiro",
 		Status: "[+1 de Destreza] [+2 em Inteligência]",
-    Icon: "brasao_ Engenheiro.png",
+    Icon: "enginer.png",
 		Descrição: "O Engenheiro fabrica e melhora itens, armas, e acessórios, além de poder fazer reparos em drones, torretas automáticas, armas, armaduras e itens. Também pode oferecer suporte médico básico e intermediário.",
 		Itens_Iniciais: "Armadura Media de Cadete (+1 Armadura, valor $50). [ Pistola B-03 (Dano 1d4 + 1, Distancia 20 Metros, valor $40). Ou Espada Curta de Bronze (Dano 1d6, valor $25) ] Canhão de ombro TX-MK1 (Dano 1d8, Distância 15 Metros, Recarga 2 turnos, valor $65). Mochila com 4 Espaços. X2 Analgésicos (cura 2d4 se ingerido). $10 de dinheiro.",
 		Habilidades_por_Nivel: "Nível 1: Boost. Nível 2: Reparos menores. Ponto Delta. Nível 3: Preparar Torreta. Aranha Robô. Nível 4: Fabricar. Curar Feridas. Hackear. Ponto Delta. Nível 5: Melhorar Nível 6: Cortador de plasma. Ponto Delta. Nível 7: Canhão Umbral. Nível 8: Reparos Maiores Ponto Delta. Nível 9: Canhão Ômega. Nível 10: Forma Delta. Ponto Delta."
@@ -409,7 +409,7 @@ const ClassDB = [
 	{
 		Classe: "Ranger",
 		Status: "[+1 de Esquiva] [+2 em Destreza]",
-    Icon: "brasao_ Engenheiro.png",
+    Icon: "ranger.png",
 		Descrição: "O  Ranger é aquele que explora e faz reconhecimento coletando informações para a equipe contendo bastante mobilidade, mas em contrapartida usa menos blindagem o que deixa o mesmo menos resistente.",
 		Itens_Iniciais: "Armadura Leve de Cadete (+1 Armadura, valor $50). [ Rifle de caça A-02 (Dano 1d10, Distância 10-30 Metros, valor $60). E Faca de Sobrevivência (Dano 1d4, valor $15) ]. Mochila com 4 Espaços. X3 Analgésicos (cura 2d4 se ingerido). X2 Granadas de Flash (Cega temporariamente quem está próximo 10 Metros de raio, tempo de cegueira 1d4 turnos). X1 Scanner. $10 de dinheiro.",
 		Habilidades_por_Nivel: "Nível 1: Drone de reconhecimento. Nível 2: Escanear. Ponto Delta. Nível 3: Queda Lenta. Camuflar. Nível 4: Overclock Ponto Delta. Nível 5: Visão Térmica. Nível 6: Radar. Ponto Delta. Nível 7: Gás Venenoso. Nível 8: Fúria. Ponto Delta. Nível 9 nEletrificar Lâmina. Nível 10: Forma Delta. 1 Ponto Delta."
